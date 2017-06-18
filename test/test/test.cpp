a793578e45816ba05e84bef875fe4e0db8b8169b
Psyche-zhe <1751235331@qq.com>
2017-06-18 10:07:27 +0800

    pointer

--- test/test/test.cpp
+++ test/test/test.cpp
@@ -3,5 +3,12 @@
 int main(int argc, char *argv[])
 {	
 	std::cout << "first test!" << std::endl;
+	int ival = 1024;
+	int *pi = &ival;
+	int **ppi = &pi;
+	std::cout << ival << "\t" << &ival << std::endl;
+	std::cout << *pi << "\t" << pi << std::endl;
+	std::cout << **ppi << "\t" << ppi << std::endl;
+
 	return 0;
 }
