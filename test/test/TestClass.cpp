1e52ff539c52e48a5343ff432c2a7b70352c4dc8
psyche <2767073900@qq.com>
2017-08-02 23:27:13 +0800

    new class

--- test/test/TestClass.cpp
+++ test/test/TestClass.cpp
@@ -1,17 +1,94 @@
 #include "TestClass.h"
+#include <iostream>
 
-
-TestClass::TestClass() :age(10), name("psyche")
+Stock::Stock()
 {
-	age = 18;
-	name = "jack";
+	std::cout << "Default constructor called!\n";
+	company = "no name";
+	shares = 0;
+	share_val = 0.0;
+	total_val = 0.0;
 }
 
-void TestClass::ShowInfo()
+Stock::Stock(const std::string & co, long n, double pr)
 {
-	std::cout << "name : "<< name << " , age " << age;
+	std::cout << "Constructor using " << co << "called\n";
+	company = co;
+
+	if (n < 0)
+	{
+		std::cout << "Number of shares can't be negative;"
+			<< company << "shares set to 0.\n";
+		shares = 0;
+	}
+	else
+	{
+		shares = n;
+	}
+	share_val = pr;
+	set_tot();
 }
 
-TestClass::~TestClass()
+Stock::~Stock()
 {
+	std::cout << "Bye, " << company << "!\n";
+}
+
+void Stock::buy(long num, double price)
+{
+	if (num < 0)
+	{
+		std::cout << "number of shares purchased can't be negative."
+			<< "Transaction is aborted.\n";
+	}
+	else
+	{
+		shares += num;
+		share_val = price;
+		set_tot();
+	}
+}
+
+void Stock::sell(long num, double price)
+{
+	using std::cout;
+	if (num < 0)
+	{
+		cout << "Number of shares sold can't be negative."
+			<< "transaction is aborted.\n";
+	}
+	else if (num > shares)
+	{
+		cout << "You can't sell more than you have!"
+			<< "Transaction is aborted!\n";
+	}
+	else
+	{
+		shares -= num;
+		share_val = price;
+		set_tot();
+	}
+}
+
+void Stock::update(double price)
+{
+	share_val = price;
+	set_tot();
+}
+
+void Stock::show()
+{
+	using std::cout;
+	using std::ios_base;
+	ios_base::fmtflags orig = cout.setf(ios_base::fixed, ios_base::floatfield);
+	std::streamsize prec = cout.precision(3);
+
+	cout << "Company: " << company
+		<< "Shares: " << shares << "\n";
+	cout << "Share Price: $ " << share_val;
+	cout.precision(2);
+	cout << "Total Worth: $" << total_val << "\n";
+
+	cout.setf(orig, ios_base::floatfield);
+	cout.precision(prec);
 }
--- test/test/TestClass.h
+++ test/test/TestClass.h
@@ -2,14 +2,19 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class TestClass
-{
-public:
-	TestClass();
-	void ShowInfo();
-	~TestClass();
+class Stock{
 private:
-	int age;
-	std::string name;
-};
-
+	std::string  company;
+	long shares;
+	double share_val;
+	double total_val;
+	void set_tot(){ total_val = shares * share_val; }
+public:
+	Stock();
+	Stock(const std::string & co, long n = 0, double pr = 0.0);
+	~Stock();
+	void buy(long num, double price);
+	void sell(long num, double price);
+	void update(double price);
+	void show();
+};	
